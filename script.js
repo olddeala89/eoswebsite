15b71ad59cd3d3686984fd1014460ee960f00d80
olddeala <113938189+olddeala89@users.noreply.github.com>
2023-11-07 13:57:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ let getup = document.getElementById("getup");
 getup.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
+
+let vk = document.getElementById("vk");
+let inst = document.getElementById("inst");
+let tg = document.getElementById("tg");
+let insturl = "https://instagram.com/eos_1993?igshid=MmVlMjlkMTBhMg==";
+
+vk.addEventListener("click", function() {
+    alert("Данная страница в разработке :)");
+});
+
+inst.addEventListener("click", function() {
+    window.open(insturl);
+});
+
+tg.addEventListener("click", function() {
+    alert("Данная страница в разработке :)");
+});
