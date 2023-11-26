7ffe933429624b13991be730edbeb597052eccb9
olddeala <113938189+olddeala89@users.noreply.github.com>
2023-11-26 21:33:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,10 +32,32 @@ window.addEventListener('click', function (event) {
         modal.style.display = 'none';
     }
 });
-let submitBtn = document.getElementById('submitBtn');
-submitBtn.addEventListener('click', function () {
-    // Добавьте здесь код для обработки отправки формы
-    // Например, отправка данных на сервер с использованием AJAX
-    alert('Форма отправлена!');
-    modal.style.display = 'none';
+
+
+//Код отправки формы
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Предотвратить отправку формы по умолчанию
+
+    // Получаем значения из полей ввода
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+
+    // Ваша логика обработки данных
+    //Валидация
+    if (name.length >= 3 && ((phone[0] == '+' && phone[1] == '7' && phone.length == 12) || (phone[0] == '8' && phone.length == 11)) ) {
+        console.log('Имя:', name);
+        console.log('Номер телефона:', phone);
+    }
+    else {
+        alert('Введите корректные данные');
+    }
+    //console.log('Имя:', name);
+    //console.log('Номер телефона:', phone);
+
+    // Дополнительные действия, например, отправка данных на сервер
+    // ...
+
+    // Очистка полей формы (если необходимо)
+    document.getElementById('name').value = '';
+    document.getElementById('phone').value = '';
 });
